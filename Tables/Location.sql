6b81dac85c9396ccc6febe0e304546b7604d9f96
burnman_ burnman_ <lukasz.kolazinski@gmail.com>
2019-10-14 14:50:44 +0200

    Region

--- Tables/Location.sql
+++ Tables/Location.sql
@@ -3,10 +3,12 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [LocationNo] INT NOT NULL, 
     [Name] VARCHAR(200) NOT NULL, 
+	[RegionId] INT NULL,
     [Telephone] VARCHAR(200) NULL, 
     [Email] VARCHAR(200) NULL, 
     [Address1] VARCHAR(200) NOT NULL, 
     [Address2] VARCHAR(200) NULL, 
     [CreationDate] DATETIME NOT NULL DEFAULT getdate(), 
-    [ModificationDate] DATETIME NULL
+    [ModificationDate] DATETIME NULL, 
+    CONSTRAINT [FK_Location_Region] FOREIGN KEY ([RegionId]) REFERENCES [Region]([Id])
 )
---
+++ Tables/Region.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Region]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RegionNo] INT NOT NULL, 
+    [Name] VARCHAR(200) NOT NULL,
+	[Description] VARCHAR(max) NULL,
+	CONSTRAINT [UK_Role_RegionNo] UNIQUE ([RegionNo])
+)
