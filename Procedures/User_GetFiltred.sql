ecd8a0065faf1331199dd1f50d78b03fff3d5b48
burnman_ burnman_ <lukasz.kolazinski@gmail.com>
2019-10-17 15:00:26 +0200

    Filtered

---
+++ Procedures/User_GetFiltred.sql
@@ -0,0 +1,66 @@
+﻿CREATE PROCEDURE [dbo].[User_GetFiltred]
+(
+	@RoleIdTable [dbo].[TVPUserType] READONLY, -- must be declared as read only
+	@DepartmentIdTable [dbo].[TVPUserType] READONLY -- must be declared as read only
+)
+AS
+BEGIN
+
+	DECLARE @RoleIsDempty INT = 1
+	IF EXISTS ( SELECT 1 FROM @RoleIdTable )
+		SET @RoleIsDempty = 0
+
+	DECLARE @DepartmentIsDempty INT = 1
+	IF EXISTS ( SELECT 1 FROM @RoleIdTable )
+		SET @DepartmentIsDempty = 0
+
+
+	SELECT
+		u.[Id],
+		u.[Login],
+		u.[Name],
+		NULL AS [Password],
+		u.[Surname],
+		u.[UserNo],
+		u.[CreationDate],
+		u.[ModifiationDate],
+		
+		u.[RoleId],
+		r.[Name] AS [RoleName],
+		r.[RoleNo],
+		
+		u.[DepartmentId],
+		d.[DepartmentNo],
+		d.[Name] AS [DepartmentName]
+		
+	FROM [dbo].[User] u
+	LEFT JOIN @RoleIdTable t ON t.[UniqueId] = u.[RoleId]
+	LEFT JOIN [dbo].[Role] r ON r.[Id] = t.[UniqueId]
+	LEFT JOIN @DepartmentIdTable w ON w.[UniqueId] = u.[DepartmentId]
+	LEFT JOIN [dbo].[Department] d ON d.[Id] = u.[DepartmentId]
+	WHERE 
+		(@RoleIsDempty = 1 OR t.[UniqueId] IS NOT NULL)
+		OR
+		(@DepartmentIsDempty = 1 OR w.[UniqueId] IS NOT NULL)
+END
+
+
+/* Test
+
+DECLARE @TmpRoleIdTable [dbo].[TVPUserType] 
+INSERT INTO @TmpRoleIdTable VALUES(1)
+INSERT INTO @TmpRoleIdTable VALUES(2)
+INSERT INTO @TmpRoleIdTable VALUES(3)
+INSERT INTO @TmpRoleIdTable VALUES(4)
+INSERT INTO @TmpRoleIdTable VALUES(5)
+
+DECLARE @TmpDepartmentIdTable [dbo].[TVPUserType] 
+INSERT INTO @TmpDepartmentIdTable VALUES(1)
+INSERT INTO @TmpDepartmentIdTable VALUES(2)
+INSERT INTO @TmpDepartmentIdTable VALUES(3)
+INSERT INTO @TmpDepartmentIdTable VALUES(4)
+INSERT INTO @TmpDepartmentIdTable VALUES(5)
+
+EXEC [dbo].[User_GetFiltred] @TmpRoleIdTable, @TmpDepartmentIdTable
+
+*/
