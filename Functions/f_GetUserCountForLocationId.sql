977ba6fd48fd88d67d590ade24df6d8a1221354a
burnman_ burnman_ <lukasz.kolazinski@gmail.com>
2019-10-22 11:20:57 +0200

    Scalars

---
+++ Functions/f_GetUserCountForLocationId.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION [dbo].[f_GetUserCountForLocationId]
+(
+	@UserId INT,
+	@RegionId INT
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @Count INT = 0
+
+	SELECT @Count = COUNT(*) 
+	FROM [dbo].[UserLocation] ul
+	INNER JOIN [dbo].[Location] l ON l.[Id] = ul.[LocationId]
+		AND (@RegionId IS NULL OR l.[RegionId] = @RegionId)
+	WHERE ul.[UserId] = @UserId
+
+	RETURN @Count
+END
