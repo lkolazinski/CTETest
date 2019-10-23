5984406b93305f3b7f0e494f62f270e534289909
burnman_ burnman_ <lukasz.kolazinski@gmail.com>
2019-10-23 12:43:47 +0200

    MERGE

--- Scripts/Imports/ImportUsersLocations.sql
+++ Scripts/Imports/ImportUsersLocations.sql
@@ -38,6 +38,68 @@
 		SELECT * FROM [UserLocation] e WHERE e.[UserId] = u.[Id] AND e.[LocationId] = l.[Id]
 	)
 
+	--DELETE FROM [dbo].[UserLocation] WHERE ID < 20000
+
+	;WITH ByLocation
+	AS
+	(
+		SELECT
+			u.[Id] AS [UserId],
+			l.[Id] AS [LocationId]
+		FROM [dbo].[User] u
+		INNER JOIN [dbo].[Location] l ON l.[Id] = u.[LocationId] 
+	),
+	ByRegion
+	AS
+	(
+		SELECT
+			u.[Id] AS [UserId],
+			l.[Id] AS [LocationId]
+		FROM [dbo].[User] u
+		INNER JOIN [dbo].[Region] r ON r.[Id] = u.[RegionId]
+		INNER JOIN [dbo].[Location] l ON l.[RegionId] = u.[RegionId]
+	),
+	ByDepartment
+	AS
+	(
+		SELECT
+			u.[Id] AS [UserId],
+			l.[Id] AS [LocationId]
+		FROM [dbo].[User] u
+		INNER JOIN [dbo].[Department] d ON d.[Id] = u.[DepartmentId]
+		INNER JOIN [dbo].[Region] r ON r.[DepartmentId] = d.[Id]
+		INNER JOIN [dbo].[Location] l ON l.[RegionId] = r.[Id]
+	),
+	AllLocations
+	AS
+	(
+		SELECT 
+		*
+		FROM ByLocation
+		UNION
+		SELECT 
+		*
+		FROM ByRegion
+		UNION
+		SELECT 
+		*
+		FROM ByDepartment
+	)
+	MERGE [dbo].[UserLocation] AS TARGET
+	USING
+	(
+		SELECT 
+		* 
+		FROM AllLocations
+	) AS SOURCE ([MyUserId], [MyLocationId])
+	ON (TARGET.[UserId] = SOURCE.[MyUserId] AND TARGET.[LocationId] = SOURCE.[MyLocationId])
+	WHEN NOT MATCHED BY SOURCE THEN
+		DELETE
+	WHEN NOT MATCHED THEN
+		INSERT ([UserId], [LocationId])
+		VALUES (SOURCE.[MyUserId], SOURCE.[MyLocationId])
+	OUTPUT deleted.*, $action, inserted.* ;
+
 	COMMIT TRANSACTION
 
 END TRY  
