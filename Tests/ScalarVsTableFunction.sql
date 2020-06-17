2b8e759dcff371004e9ff4da8c23995d9153b64c
Lukasz Kolazinski <lkolazinski@sii.pl>
2020-06-17 18:17:50 +0200

    Adding missing one

---
+++ Tests/ScalarVsTableFunction.sql
@@ -0,0 +1,16 @@
+﻿select 
+	[dbo].[f_GetLocationCountForUserId] (U.Id) AS LocationCount,
+	U.Id,
+	U.Name + ' ' +U.Surname
+ from [dbo].[USER] U
+ WHERE U.Id BETWEEN 100 AND 999999999
+ ORDER BY 1 desc
+
+ select 
+	t.LocationCount,
+	U.Id,
+	U.Name + ' ' +U.Surname
+ from [dbo].[USER] U
+ CROSS APPLY [ft_GetLocationCountForUserId] (U.Id) t
+ WHERE U.Id BETWEEN 100 AND 999999999
+ ORDER BY 1 desc
